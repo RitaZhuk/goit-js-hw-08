d8735dcceb2beec8c9f90bf9eec2bea16340f84c
RitaZhuk <ritochka199097@gmail.com>
2022-08-19 22:39:30 +0300

    *storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import storageApp from "./storage"
+console.log(storageApp);
 
+const formRef = document.querySelector(".feedback-form");
+console.log(formRef);
+formRef.addEventListener('input', handleInput);
+
+const formData = {};
+
+function handleInput(event) {
+    const {name, value} = event.target;
+    formData[name] = value;
+    console.log(formData);
+}
